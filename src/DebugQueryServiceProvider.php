50b07ae08ebc4e1147afa72dee39384d6d17d870
Ahmed Hashem <a.mohamed.salah.hashem@gmail.com>
2022-03-23 20:55:20 +0200

    🚀🔥Compatibility with Laravel 9 🔥🚀

--- src/DebugQueryServiceProvider.php
+++ src/DebugQueryServiceProvider.php
@@ -10,11 +10,17 @@ class DebugQueryServiceProvider extends ServiceProvider
     
     public function register()
     {
+        
         Builder::macro('sql', function () {
-            $bindings = $this->getBindings();
+            $bindings = array_map(
+                fn ($value) => is_numeric($value) ? $value : "'{$value}'",
+                $this->getBindings()
+            );
+
             $query = $this->toSql();
             $sql = Str::replaceArray('?', $bindings, $query);
             dd($sql);
         });
+
     }
 }
